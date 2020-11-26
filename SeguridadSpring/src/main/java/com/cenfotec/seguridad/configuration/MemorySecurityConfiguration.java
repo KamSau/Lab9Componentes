848cf0682899f0ca975c0b21c1b61e4dfe0be468
Kamil Sauma <kamilsauma07@gmail.com>
2020-11-26 14:25:40 -0600

    Fin

---
+++ SeguridadSpring/src/main/java/com/cenfotec/seguridad/configuration/MemorySecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.cenfotec.seguridad.configuration;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@EnableWebSecurity
+public class MemorySecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws
+	Exception {
+	String encodedPassword = new BCryptPasswordEncoder().encode("kamil");
+
+	auth.inMemoryAuthentication()
+	 .withUser("kamil")
+	 .password(encodedPassword)
+	 .roles("manager", "dev")
+	 .and().passwordEncoder(new BCryptPasswordEncoder());
+	 }
+	
+}
